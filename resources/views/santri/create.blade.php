9d66c9a792ff711fde569699f689a34d4ae8d38c
ozankaze <ozankaze101@gmail.com>
2018-08-28 14:43:49 +0700

    create santri

--- resources/views/santri/create.blade.php
+++ resources/views/santri/create.blade.php
@@ -2,29 +2,35 @@
 
 
 @section('content')
-<form action="/santri" method="POST">
-    <div class="card">
-        @csrf
-        <div class="card-header">
-            <div class="card-title">Form Nama Santri</div>
-        </div>
-        <div class="card-body">
-            <div class="form-group">
-                <label for="squareInput">Nama</label>
-                <input name="name" type="text" class="form-control input-square" id="squareInput" placeholder="Square Input">
+<div class="content">
+    <div class="container-fluid">
+        <h4 class="page-title">Form</h4>
+        <form action="/santri" method="POST">
+            <div class="card">
+                @csrf
+                <div class="card-header">
+                    <div class="card-title">Form Nama Santri</div>
+                </div>
+                <div class="card-body">
+                    <div class="form-group">
+                        <label for="squareInput">Name</label>
+                        <input name="name" type="text" class="form-control input-square" id="squareInput" placeholder="Masukan Name">
+                    </div>
+                    <div class="form-group">
+                        <label for="squareInput">Address</label>
+                        <input name="address" type="text" class="form-control input-square" id="squareInput" placeholder="Masukan Address">
+                    </div>
+                    <div class="form-group">
+                        <label for="squareInput">Status</label>
+                        <input name="status" type="text" class="form-control input-square" id="squareInput" placeholder="Jabatan">
+                    </div>										
+                </div>
+                <div class="card-action">
+                    <button class="btn btn-success">Submit</button>
+                </div>
             </div>
-            <div class="form-group">
-                <label for="squareInput">Alamat</label>
-                <input name="address" type="text" class="form-control input-square" id="squareInput" placeholder="Square Input">
-            </div>
-            <div class="form-group">
-                <label for="squareInput">Status</label>
-                <input name="status" type="text" class="form-control input-square" id="squareInput" placeholder="Square Input">
-            </div>										
-        </div>
-        <div class="card-action">
-            <button class="btn btn-success">Submit</button>
-        </div>
+        </form>
     </div>
-</form>
+</div>
+
 @endsection
